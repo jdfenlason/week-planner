f9fbcb705dfd304da2feaaab7114b8cd1d3cd228
Henry Nichols <nicholsh562@gmail.com>
2021-04-02 13:29:06 -0700

    End of Henry's turn

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var $modal = document.querySelector('.modal-container');
+var $modalContent = document.querySelector('.modal-content');
 var $overlay = document.querySelector('.overlay');
 var $submitBtn = document.querySelector('.submit-btn');
 var $entryBtn = document.querySelector('.entry-btn');
@@ -8,11 +9,13 @@ var upDown = false;
 $entryBtn.addEventListener('click', function (e) {
   if (upDown === false) {
     $modal.className = 'modal-container hidden';
+    $modalContent.className = 'modal-content hidden';
     $overlay.className = 'overlay hidden';
     $container.className = 'container';
     upDown = true;
   } else {
     $modal.className = 'modal-container';
+    $modalContent = 'modal-content';
     $overlay.className = 'overlay';
     $container.className = 'container hidden';
     upDown = false;
