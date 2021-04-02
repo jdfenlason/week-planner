b87b4f64ef67f3966abeaa6c3f1a2bf0ad4f63ff
Steve-A-Dore <Steve.A.Dore@outlook.com>
2021-04-02 14:49:24 -0700

    steve commit 3

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 var $modal = document.querySelector('.modal-container');
 var $modalContent = document.querySelector('.modal-content');
 var $overlay = document.querySelector('.overlay');
-var $submitBtn = document.querySelector('.submit-btn');
+var $submitBtn = document.querySelector('.submit-button');
 var $entryBtn = document.querySelector('.entry-btn');
 var $container = document.querySelector('.container');
+var $form = document.querySelector('form');
 var upDown = false;
 
 $entryBtn.addEventListener('click', function (e) {
   if (upDown === false) {
+    $container.className = 'container';
     $modal.className = 'modal-container hidden';
     $modalContent.className = 'modal-content hidden';
     $overlay.className = 'overlay hidden';
-    $container.className = 'container';
     upDown = true;
   } else {
     $modal.className = 'modal-container';
-    $modalContent = 'modal-content';
+    $modalContent.className = 'modal-content';
     $overlay.className = 'overlay';
     $container.className = 'container hidden';
     upDown = false;
   }
+});
 
+$form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  console.log(e);
+  console.log('hello');
 });
