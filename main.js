c3a21270bad27c09f16660cab7236783c8d028d8
Steve-A-Dore <Steve.A.Dore@outlook.com>
2021-04-02 12:59:44 -0700

    Steve commit 2

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 var $modal = document.querySelector('.modal-container');
+var $overlay = document.querySelector('.overlay');
 var $submitBtn = document.querySelector('.submit-btn');
 var $entryBtn = document.querySelector('.entry-btn');
+var upDown = false;
 
 $entryBtn.addEventListener('click', function (e) {
-  $modal.className('.modal-container');
+  if (upDown === false) {
+    $modal.className = 'modal-container hidden';
+    $overlay.className = 'overlay hidden';
+    upDown = true;
+  } else {
+    $modal.className = 'modal-container';
+    $overlay.className = 'overlay';
+    upDown = false;
+  }
 
 });
