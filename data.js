a2cd70b880a5518bf3a881b4bd2a840bb71aca7b
Jake <jdfenlason@gmail.com>
2021-04-02 14:12:03 -0400

    end of jake turn

---
+++ data.js
@@ -0,0 +1,37 @@
+var planner = [
+  {
+    day: 'Sunday';
+    time: [];
+    description: [];
+  },
+  {
+    day: 'Monday';
+    time: [];
+    description: [];
+  },
+  {
+    day: 'Tuesday';
+    time: [];
+    description: [];
+  },
+  {
+    day: 'Wednesday';
+    time: [];
+    description: [];
+  },
+  {
+    day: 'Thursday';
+    time: [];
+    description: [];
+  },
+  {
+    day: 'Friday';
+    time: [];
+    description: [];
+  },
+  {
+    day: 'Saturday';
+    time: [];
+    description: [];
+  }
+];
