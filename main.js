878a6da073bf08a20c476f907eebe67bcf128ab5
Henry Nichols <nicholsh562@gmail.com>
2021-04-02 12:50:59 -0700

    New Code Henry

---
+++ main.js
@@ -0,0 +1,8 @@
+var $modal = document.querySelector('.modal-container');
+var $submitBtn = document.querySelector('.submit-btn');
+var $entryBtn = document.querySelector('.entry-btn');
+
+$entryBtn.addEventListener('click', function (e) {
+  $modal.className('.modal-container');
+
+});
